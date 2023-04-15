c8b64fa8ec16ebd203a7e47a981c7142cad46af7
Himanshu Kumar <himanshukumar_dr@srmuniv.edu.in>
2023-04-15 19:28:57 -0400

    add pre commit config file

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[required_packages],
     extras_require={
-        "dev": docs_packages + style_packages,
+        "dev": docs_packages + style_packages + test_packages ["pre-commit==2.19.0"],
         "docs": docs_packages,
         "test": test_packages,
     },
